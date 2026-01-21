e512bf589b790a12e40da83a900cefee5a22aa14
Vivek Jaiswal <vivekjaiswalcode@gmail.com>
2026-01-21 11:21:14 +0530

    google-authentication

---
+++ src/lib/authOption.ts
@@ -0,0 +1,11 @@
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+export const authOption : NextAuthOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret : process.env.GOOGLE_CLIENT_SECRET as string
+        })
+    ]
+}
